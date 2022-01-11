8a31be4577bd94c8a7f24a72253ce83b49bf6aae
lassepe <lasse.peters@mailbox.org>
2022-01-11 10:24:58 +0100

    All strategies accept time as a second input

--- src/solver.jl
+++ src/solver.jl
@@ -1,9 +1,9 @@
 """
-    solve_trajectory_game!(solver, game, state)
+    solve_trajectory_game!(solver, game, initial_state)
 
 Computes a collection of strategies `γs`, one for each player of the game. This call may modify \
 the `solver` itself; e.g., due to learning of parameters or updates of initial guesses for \
-repeated calls. That is, a subsequent call of `solve_trajectory_game!` on the same `state` \
+repeated calls. That is, a subsequent call of `solve_trajectory_game!` on the same `initial_state` \
 may result in a different strategy.
 """
 function solve_trajectory_game! end
--- src/strategy.jl
+++ src/strategy.jl
@@ -1,9 +1,9 @@
 """
-    strategy(state)
+    strategy(state, time)
 
-Computes an `action` for this player for the given `state`. Note that the `strategy` may be
-non-deterministic. Hence, unless it is seeded to the same pseudo-random state, it subsequent calls
-of `strategy` may yield different actions.
+Computes an `action` for this player for the given `state` and `time`. Note that the `strategy` may
+be non-deterministic. Hence, unless it is seeded to the same pseudo-random state, it subsequent
+calls of `strategy` may yield different actions.
 """
 abstract type AbstractStrategy end
 
