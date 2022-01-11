7b95d6bed587b31d3fdb1f426ba537ead080117f
lassepe <lasse.peters@mailbox.org>
2022-01-11 19:16:50 +0100

    Cost abstraction

--- src/costs.jl
+++ src/costs.jl
@@ -1,11 +1,46 @@
-abstract type AbstractCostStructure end
-
 "A trait type that indicates zero sum payoff structure of a game."
-struct ZeroSumCostStructure{TC} <: AbstractCostStructure
-    cost::TC
+abstract type AbstractCostStructure end
+struct ZeroSumCostStructure <: AbstractCostStructure end
+struct GeneralSumCostStructure <: AbstractCostStructure end
+
+"""
+A time-separable cost representation that supports different
+"""
+struct TrajectoryGameCost{TC<:AbstractCostStructure,TS,TR}
+    cost_structure::TC
+    "A vector of callable stage cost, where `stage_costs[ii](x, u, t) -> Real` yields the cost for \
+    player `ii` at a single state `x`, input `u`, and time `t`."
+    stage_costs::TS
+    "A vector of callable reduction functions that combine sequence of costs. For example, `sum` \
+    computs the sum of stage costs."
+    reducers::TR
+
+    function TrajectoryGameCost(
+        cost_structure::ZeroSumCostStructure,
+        stage_cost,
+        reducer = sum,
+    ) where {TS,TR}
+        !(stage_cost isa AbstractVector) || error(
+            "ZeroSumCostStructure does not accept vectors of costs. Pass a single callable for \
+            `stage_cost` and `reducer` instead",
+        )
+
+        stage_costs = (stage_cost, (-) ∘ stage_cost)
+        reducers = (reducer, reducer)
+        new{ZeroSumCostStructure,typeof(stage_costs),typeof(reducers)}(
+            cost_structure,
+            stage_costs,
+            reducers,
+        )
+    end
 end
 
-struct GeneralSumCostStructure{TC<:Vector} <: AbstractCostStructure
-    cost::TC
+"""
+Computes the cost over the full *sequence* of state `xs` and inputs `us`.
+"""
+function (cost::TrajectoryGameCost)(player_i, xs, us)
+    ts = eachindex(xs)
+    Iterators.map(xs, us, ts) do x, u, t
+        cost.stage_costs[player_i](x, u, t)
+    end |> cost.reducers[player_i]
 end
-
--- src/game.jl
+++ src/game.jl
@@ -1,20 +1,18 @@
 Base.@kwdef struct TrajectoryGame{
-    TC<:AbstractCostStructure,
+    TCS<:AbstractCostStructure,
+    TC<:TrajectoryGameCost{TCS},
     TD<:AbstractDynamics,
-    TE<:AbstractEnvironment,
-    TH,
+    TE<:AbstractEnvironment
 }
     "A cost structure that holds information about the payoff structure."
-    cost_structure::TC
+    cost::TC
     "The environment object that characerizes static constraints of the problem and can be used \
     for visualization."
     dynamics::TD
     "An environment object that holds information about constraints and can be used over visualization"
     env::TE
-    "The number of time steps in the game"
-    horizon::TH
 end
 
 function num_players(g::TrajectoryGame)
-    length(g.cost_structure.cost)
+    length(g.cost.reducers)
 end
