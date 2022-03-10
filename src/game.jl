58c2a08bb058ee7334a35f13c6de3a607bb7fd12
lassepe <lasse.peters@mailbox.org>
2022-03-10 23:03:10 +0100

    A rapid prototype of primal-dual constraint handling via dual generators.

--- src/game.jl
+++ src/game.jl
@@ -2,6 +2,7 @@ Base.@kwdef struct TrajectoryGame{
     TD<:AbstractDynamics,
     TC,
     TE<:AbstractEnvironment,
+    TS
 }
     "A cost function taking a trajectory pairing (xs, us) in Blocks and gives the cost per player in a tuple."
     cost::TC
@@ -9,6 +10,14 @@ Base.@kwdef struct TrajectoryGame{
     dynamics::TD
     "The environment object that characerizes static constraints of the problem and can be used for visualization."
     env::TE
+    """
+    TODO: this probably should have the same interface as `cost`.
+
+    An iterable collection of intra-trajectory constraints. Each intra-trajectory constraint
+    `con(τs...) -> gs` maps a variable number trajectories to an iterable collection of scalars `gs`
+    which are positive non-negative when all constraints between these trajectories are satsifies.
+    """
+    shared_constraints::TS
 end
 
 function num_players(g::TrajectoryGame)
