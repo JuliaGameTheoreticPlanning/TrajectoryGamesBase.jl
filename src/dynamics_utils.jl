169004d432a50bc985765e1786b6ac73d53e841f
lassepe <lasse.peters@mailbox.org>
2022-03-15 18:53:15 +0100

    Address Maxi review comments

--- src/dynamics_utils.jl
+++ src/dynamics_utils.jl
@@ -4,22 +4,22 @@ applying the inputs dictated by the strategy.
 
 Kwargs:
 
-- `getinfo` is a callback `(γ, x, t) -> info` that can be passed to extract additional info from the strategy `γ` for
+- `get_info` is a callback `(γ, x, t) -> info` that can be passed to extract additional info from the strategy `γ` for
 each rollout state `x` and time `t`.
 
 Returns a tuple of collections over states `xs`, inputs `us`, and extra information `infos`.
 """
-function rollout(dynamics, strategy, x1, T = horizon(dynamics); getinfo = (γ, x, t) -> nothing)
+function rollout(dynamics, strategy, x1, T = horizon(dynamics); get_info = (γ, x, t) -> nothing)
     xs = sizehint!([x1], T)
     us = sizehint!([strategy(x1, 1)], T)
-    infos = sizehint!([getinfo(strategy, x1, 1)], T)
+    infos = sizehint!([get_info(strategy, x1, 1)], T)
 
     for tt in 1:(T - 1)
         xp = dynamics(xs[tt], us[tt], tt)
         push!(xs, xp)
         up = strategy(xp, tt + 1)
         push!(us, up)
-        infop = getinfo(strategy, x1, tt + 1)
+        infop = get_info(strategy, x1, tt + 1)
         push!(infos, infop)
     end
 
--- src/strategy_utils.jl
+++ src/strategy_utils.jl
@@ -19,16 +19,25 @@ Base.@kwdef struct RecedingHorizonStrategy{TS,TG,TSK}
     game::TG
     solve_kwargs::TSK = (;)
     receding_horizon_strategy::Ref{Any} = Ref{Any}()
-    time_last_updated::Ref{Int} = Ref{Int}()
+    time_last_updated::Ref{Int} = Ref{Int}(0)
     turn_length::Int = 10
 end
 
 function (strategy::RecedingHorizonStrategy)(state, time)
-    if mod1(time, strategy.turn_length) == 1 && strategy.time_last_updated != time
+    plan_exists = isdefined(strategy.receding_horizon_strategy, 1)
+    plan_is_still_valid = (
+        strategy.time_last_updated[] <=
+        time <=
+        strategy.time_last_updated[] + strategy.turn_length - 1
+    )
+
+    update_plan = !plan_exists || !plan_is_still_valid
+    if update_plan
         strategy.receding_horizon_strategy[] =
             solve_trajectory_game!(strategy.solver, strategy.game, state; strategy.solve_kwargs...)
         strategy.time_last_updated[] = time
     end
 
-    strategy.receding_horizon_strategy[](state, time - strategy.time_last_updated[] + 1)
+    time_along_plan = time - strategy.time_last_updated[] + 1
+    strategy.receding_horizon_strategy[](state, time_along_plan)
 end
