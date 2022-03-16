89756f9bb8eb11090008b3ee40bfd11d90fb9bf8
lassepe <lasse.peters@mailbox.org>
2022-03-16 16:20:02 +0100

    Streamline coupling constraint representation

--- src/game.jl
+++ src/game.jl
@@ -1,9 +1,4 @@
-Base.@kwdef struct TrajectoryGame{
-    TD<:AbstractDynamics,
-    TC,
-    TE<:AbstractEnvironment,
-    TS
-}
+Base.@kwdef struct TrajectoryGame{TD<:AbstractDynamics,TC,TE<:AbstractEnvironment,TS}
     "A cost function taking a trajectory pairing (xs, us) in Blocks and gives the cost per player in a tuple."
     cost::TC
     "An object that describes the dynamics of this trajectory game"
@@ -11,13 +6,11 @@ Base.@kwdef struct TrajectoryGame{
     "The environment object that characerizes static constraints of the problem and can be used for visualization."
     env::TE
     """
-    TODO: this probably should have the same interface as `cost`.
-
-    An iterable collection of intra-trajectory constraints. Each intra-trajectory constraint
-    `con(τs...) -> gs` maps a variable number trajectories to an iterable collection of scalars `gs`
-    which are positive non-negative when all constraints between these trajectories are satsifies.
+    An object which encodes the constraints between different players. It must be callable as
+    `con(xs, us) -> gs`: returning a collection of scalar constraints `gs` each of which is negative
+    if the corresponding contraint is active.
     """
-    shared_constraints::TS
+    coupling_constraints::TS
 end
 
 function num_players(g::TrajectoryGame)
