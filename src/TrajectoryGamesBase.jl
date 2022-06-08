cad83421b05953690304884f49c6791a2bdeecaa
lassepe <lasse.peters@mailbox.org>
2022-06-08 12:33:30 +0200

    Clean up TrajectoryGamesBase and export API

--- src/TrajectoryGamesBase.jl
+++ src/TrajectoryGamesBase.jl
@@ -5,17 +5,51 @@ using InfiniteArrays: Fill, ∞
 using SparseArrays: blockdiag, sparse
 using Makie: Makie, @recipe
 using Colors: @colorant_str
+using LazySets: LazySets
 
 include("visualize.jl")
+export visualize!
+
 include("dynamics.jl")
+export AbstractTemporalStructureTrait,
+    TimeInvariant,
+    TimeVarying,
+    control_bounds,
+    control_dim,
+    horizon,
+    linearize,
+    num_players,
+    rollout,
+    state_bounds,
+    state_dim,
+    temporal_structure_trait
+
 include("product_dynamics.jl")
+export ProductDynamics
+
 include("linear_dynamics.jl")
-include("dynamics_utils.jl")
+export LinearDynamics
+
 include("costs.jl")
+export AbstractCostStructure,
+    ZeroSumCostStructure,
+    GeneralSumCostStructure,
+    cost_structure_trait,
+    TrajectoryGameCost,
+    TimeSeparableTrajectoryGameCost
+
 include("environment.jl")
+export get_constraints, PolygonEnvironment
+
 include("game.jl")
+export TrajectoryGame
+
 include("strategy.jl")
-include("strategy_utils.jl")
+export AbstractStrategy, #
+    JointStrategy,
+    RecedingHorizonStrategy,
+    join_actions
+
 include("solve.jl")
 
 end
--- src/dynamics.jl
+++ src/dynamics.jl
@@ -50,3 +50,33 @@ Linearize about a trajectory `(x, u)` with `x` and and `u` layed out as `Vector{
 indexed).
 """
 function linearize end
+
+#=== generic utils ===#
+
+"""
+Simulates a `strategy` by evolving the `dynamics` for `T` time steps starting from state `x1` and
+applying the inputs dictated by the strategy.
+
+Kwargs:
+
+- `get_info` is a callback `(γ, x, t) -> info` that can be passed to extract additional info from \
+the strategy `γ` for each rollout state `x` and time `t`.
+
+Returns a tuple of collections over states `xs`, inputs `us`, and extra information `infos`.
+"""
+function rollout(dynamics, strategy, x1, T = horizon(dynamics); get_info = (γ, x, t) -> nothing)
+    xs = sizehint!([x1], T)
+    us = sizehint!([strategy(x1, 1)], T)
+    infos = sizehint!([get_info(strategy, x1, 1)], T)
+
+    for tt in 1:(T - 1)
+        xp = dynamics(xs[tt], us[tt], tt)
+        push!(xs, xp)
+        up = strategy(xp, tt + 1)
+        push!(us, up)
+        infop = get_info(strategy, xs[tt], tt + 1)
+        push!(infos, infop)
+    end
+
+    (; xs, us, infos)
+end
