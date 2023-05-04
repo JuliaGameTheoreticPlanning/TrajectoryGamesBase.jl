247aa7214a727a3c21c98afba9fa97f6d5ef7d19
lassepe <lasse.peters@mailbox.org>
2023-05-04 14:44:50 +0200

    Rollout supports skipping the last strategy call

--- src/dynamics.jl
+++ src/dynamics.jl
@@ -54,17 +54,34 @@ Kwargs:
 
 - `get_info` is a callback `(γ, x, t) -> info` that can be passed to extract additional info from \
 the strategy `γ` for each rollout state `x` and time `t`.
+- `skip_last_strategy_call = false`. Setting this to true avoids calling the strategy on the last \
+time step because this input will never be applied anyway. In that case, us will have one element \
+less than xs.
 
 Returns a tuple of collections over states `xs`, inputs `us`, and extra information `infos`.
 """
-function rollout(dynamics, strategy, x1, T = horizon(dynamics); get_info = (γ, x, t) -> nothing)
+function rollout(
+    dynamics,
+    strategy,
+    x1,
+    T = horizon(dynamics);
+    get_info = (γ, x, t) -> nothing,
+    skip_last_strategy_call = false,
+)
     xs = sizehint!([x1], T)
     us = sizehint!([strategy(x1, 1)], T)
     infos = sizehint!([get_info(strategy, x1, 1)], T)
 
-    for tt in 1:(T - 1)
+    time_steps = 1:(T - 1)
+
+    for tt in time_steps
         xp = dynamics(xs[tt], us[tt], tt)
         push!(xs, xp)
+
+        if skip_last_strategy_call && tt == lastindex(time_steps)
+            break
+        end
+
         up = strategy(xp, tt + 1)
         push!(us, up)
         infop = get_info(strategy, xs[tt], tt + 1)
